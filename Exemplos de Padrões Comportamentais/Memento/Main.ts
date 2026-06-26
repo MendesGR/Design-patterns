95304a9bdb6fe5ef69b62aefd61dd8e80341fd88
Gabriel Mendes <102915068+MendesGR@users.noreply.github.com>
2026-06-26 18:33:13 -0300

    Add files via upload

---
+++ Exemplos de Padrões Comportamentais/Memento/Main.ts
@@ -0,0 +1,26 @@
+import { ImageEditor } from "./ImageEditor";
+import { ImageEditorBackupManager } from "./ImageEditorBackupManager";
+
+const imageEditor = new ImageEditor("/media/image.png", "png");
+const backupManager = new ImageEditorBackupManager(imageEditor);
+
+backupManager.backup();
+imageEditor.convertFormatTo("jpg");
+
+backupManager.backup();
+imageEditor.convertFormatTo("bmp");
+
+backupManager.backup();
+imageEditor.convertFormatTo("gif");
+
+console.log(imageEditor); // gif
+
+backupManager.undo();
+console.log(imageEditor); // bmp
+
+backupManager.undo();
+console.log(imageEditor); // jpg
+
+backupManager.undo();
+console.log(imageEditor); // png
+
